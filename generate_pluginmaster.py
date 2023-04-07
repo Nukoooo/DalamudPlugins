208f4059bb14e5903919482c8482b845bab52ea3
Nukoooo <89713806+Nukoooo@users.noreply.github.com>
2023-04-07 11:33:40 +0800

    Update generate_pluginmaster.py

--- generate_pluginmaster.py
+++ generate_pluginmaster.py
@@ -6,7 +6,7 @@ from os.path import getmtime
 from zipfile import ZipFile, ZIP_DEFLATED
 
 BRANCH = os.environ['GITHUB_REF'].split('refs/heads/')[-1]
-DOWNLOAD_URL = 'https://github.com/NukoOoOoOoO/DalamudPlugins/raw/{branch}/plugins/{plugin_name}/latest.zip'
+DOWNLOAD_URL = 'https://github.com/Nukoooo/DalamudPlugins/raw/{branch}/plugins/{plugin_name}/latest.zip'
 
 DEFAULTS = {
     'IsHide': False,
